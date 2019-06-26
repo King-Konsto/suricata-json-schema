81fd6e22b97bfb246f4f6f521125f4618a6cdc70
Sascha Steinbiss <satta@debian.org>
2019-06-26 15:18:27 +0200

    add build scripts

---
+++ build/build-suricata-2.0.7.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+git clone https://github.com/OISF/suricata.git
+cd suricata
+git checkout suricata-2.0.7
+git clone https://github.com/OISF/libhtp.git
+./autogen.sh
+./configure --enable-hiredis --enable-geoip --enable-luajit --prefix=/usr/ --sysconfdir=/etc/ --localstatedir=/var/
+make install-full
+ldconfig
---
+++ build/build-suricata-4.0.6.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+git clone https://github.com/OISF/suricata.git
+cd suricata
+git checkout suricata-4.0.6
+git clone https://github.com/OISF/libhtp.git
+./autogen.sh
+./configure --enable-hiredis --enable-geoip --enable-luajit --prefix=/usr/ --sysconfdir=/etc/ --localstatedir=/var/
+make install-full
+ldconfig
