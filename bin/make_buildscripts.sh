84bdc99c4d052431223408871ed4925ecd07afac
Konstantin Klinger <konstantin.klinger@dcso.de>
2019-08-06 11:51:13 +0200

    create new folder structure and adjust build script

--- bin/make_buildscripts.sh
+++ bin/make_buildscripts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for ver in `./get_releases.sh`; do cp build/build.sh.in build/build-$ver.sh; sed -i "s/suricata_version/$ver/" build/build-$ver.sh; chmod +x build/build-$ver.sh;  done
+for ver in `./get_releases.sh`; do mkdir ../versions/$ver; mkdir ../versions/$ver/build; mkdir ../versions/$ver/logs; mkdir ../versions/$ver/schema; mkdir ../versions/$ver/configs; cp ./build.sh.in ../versions/$ver/build/build-$ver.sh; sed -i "s/suricata_version/$ver/" ../versions/$ver/build/build-$ver.sh; chmod +x ../versions/$ver/build/build-$ver.sh;  done
