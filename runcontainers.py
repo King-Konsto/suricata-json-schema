410cf7a9479567788a1b090cca6bcd7ed83d12b8
Sascha Steinbiss <satta@debian.org>
2019-09-10 17:11:13 +0200

    allow to interrupt parallel docker execution

--- runcontainers.py
+++ runcontainers.py
@@ -4,6 +4,12 @@ import docker
 import logging
 import threading
 import os
+from signal import *
+import sys
+import time
+
+logging.basicConfig(level=logging.INFO)
+
 
 f = []
 for (dirpath, dirnames, filenames) in os.walk("./versions"):
@@ -15,14 +21,18 @@ logging.info(f)
 
 client = docker.from_env()
 
+do_stop = False
 def work(versions):
+    global do_stop
     while True:
+        if do_stop:
+            logging.info("stopping worker, stop was set")
+            break
         try:
             v = versions.pop()
             run_version(v)
         except IndexError:
             break
-        print(v)
 
 def run_version(sversion):
     config_path = os.path.realpath('./versions/%s/configs' % sversion)
@@ -36,18 +46,41 @@ def run_version(sversion):
     volumes[logs_path] = {'bind': '/logs', 'mode':'rw'}
     volumes[rules_path] = {'bind': '/rules', 'mode':'ro'}
     volumes[bin_path] = {'bind': '/mybin', 'mode':'ro'}
+
+    entry_script = "/mybin/run.sh"
+    if sversion >= "suricata-4.1.0":
+        entry_script = "/mybin/run_post4.1.sh"
+
+    logging.info(entry_script)
     
     cont = client.containers.create('satta/%s' % sversion,
                                     command="/bin/bash",
                                     tty=True,
-                                    stdin_open=True,
                                     volumes=volumes)
     cont.start()
-    log = cont.exec_run("/mybin/run.sh", stderr=True, stdout=True)
+    val, log = cont.exec_run(entry_script, stderr=True, stdout=True, stream=True)
     for line in log:
-            print(line)
+        print(line)
     cont.stop()
     cont.remove()
+
+def stop_containers(*args):
+    global do_stop
+    do_stop = True
+    logging.info("stopped via sig, containers %s", client.containers.list())
+    for c in client.containers.list():
+        logging.info("stopping %s", c)
+        try:
+            c.stop()
+        except docker.errors.NotFound:
+            pass
+    for t in threads:
+        t.join()
+    logging.info("stopping done")
+    sys.exit(0)
+
+for sig in (SIGABRT, SIGILL, SIGINT, SIGSEGV, SIGTERM):
+    signal(sig, stop_containers)
 
 threads = []
 for i in range(nof_threads):
