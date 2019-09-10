a55351169fb20abc5455741014d3115791bfbe98
Konstantin Klinger <konstantin.klinger@dcso.de>
2019-09-10 14:53:23 +0200

    small adjustments

--- bin/run.sh
+++ bin/run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-mkdir -p /logs/suricata-2.0/
-ls -Al /logs/suricata-2.0/
-rm -v /logs/suricata-2.0/*
 for pcap in `find /pcaps -type f -name '*.pcap*'`; do
   echo "$pcap"
-  suricata -r $pcap -c /configs/suricata-2.0.yaml -S /rules/all.rules -l /logs/suricata-2.0;
+  suricata -r $pcap -c /configs/suricata.yaml -l /logs/ -k none
 done
