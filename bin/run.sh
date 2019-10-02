741e8fb299d6d4ee51d568cc1f6cd34401131ba5
Konstantin Klinger <konstantin.klinger@dcso.de>
2019-10-02 08:35:12 +0200

    add run with --simulate-ips to get drop even_type

--- bin/run.sh
+++ bin/run.sh
@@ -4,3 +4,6 @@ for pcap in `find /pcaps -type f -name '*.pcap*'`; do
   echo "$pcap"
   suricata -r $pcap -c /configs/suricata.yaml -l /logs/ -k none
 done
+
+echo "--simulate-ips with /pcaps/http_drop.pcap"
+suricata -r /pcaps/http_drop.pcap -c /configs/suricata.yaml -l /logs/ -k none --simulate-ips
