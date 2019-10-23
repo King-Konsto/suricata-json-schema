5a5c88f5d553d94f28ebf1439d8aca2d92d0f341
Sascha Steinbiss <satta@debian.org>
2019-10-23 11:05:34 +0200

    use released 5.0

--- versions/suricata-5.0/build/build-suricata-5.0.sh
+++ versions/suricata-5.0/build/build-suricata-5.0.sh
@@ -4,7 +4,7 @@ set -e
 apt-get install libmaxminddb-dev -y
 git clone https://github.com/OISF/suricata.git
 cd suricata
-git checkout suricata-5.0.0-rc1
+git checkout suricata-5.0.0
 git clone https://github.com/OISF/libhtp.git
 ./autogen.sh
 ./configure --enable-hiredis --enable-geoip --enable-luajit --prefix=/usr/ --sysconfdir=/etc/ --localstatedir=/var/
